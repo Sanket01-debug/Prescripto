95c165af39f4593eead11122737add5371318ace
Sanket01-debug <sanketkansal2001@gmail.com>
2025-11-01 16:52:54 +0530

    feat: add MongoDB driver updates and new operations

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/mongodb.js'
 
 //app config
 const app = express()
 const port = process.env.PORT || 4000
+connectDB()
 
 //middlewares
 app.use(express.json())
@@ -16,4 +18,4 @@ app.get('/',(req,res)=>{
     res.send('API WORKING')
 })
 
-app.listen(port, ()=> console.log("Server Started". port))
+app.listen(port, ()=> console.log("Server Started",port))
