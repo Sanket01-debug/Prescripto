321ed6c8828623ba49e0a2415d530ea36d8f6003
Sanket01-debug <sanketkansal2001@gmail.com>
2025-11-02 19:04:44 +0530

    feat: implement doctor management features with Cloudinary integration

---
+++ backend/middlewares/multer.js
@@ -0,0 +1,12 @@
+import multer from 'multer'
+
+
+const storage = multer.diskStorage({
+    filename: function(req,file,callback){
+        callback(null, this.filename.origin)
+    }
+})
+
+const upload = multer({storage})
+
+export default upload
