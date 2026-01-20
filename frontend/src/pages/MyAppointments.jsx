818f7219634e1cb5aa0f88a78d3df2bc8098580c
Sanket01-debug <sanketkansal2001@gmail.com>
2026-01-20 19:03:22 +0530

    feat: verifying razorpay functionality

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import axios from 'axios'
 import { toast } from 'react-toastify'
 import { useEffect } from 'react'
+import {useNavigate} from 'react-router-dom'
 
 const MyAppointments = () => {
 
@@ -11,6 +12,8 @@ const MyAppointments = () => {
 
   const [appointments, setAppointments] = useState([])
   const months = ["","Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+  const navigate = useNavigate()
 
   const slotDateFormat = (slotDate) => {
     const dateArray = slotDate.split('_')
@@ -65,6 +68,19 @@ const MyAppointments = () => {
       handler: async (response) => {
         console.log(response)
 
+        try {
+
+          const {data} = await axios.post(backendUrl+'/api/user/verifyRazorpay',response,{headers:{token}})
+          if (data.success) {
+            getUserAppointments()
+            navigate('/my-appointments')
+          }
+          
+        } catch (error) {
+          console.log(error)
+          toast.error(error.message)
+        }
+
       } 
     }
 
@@ -84,7 +100,8 @@ const MyAppointments = () => {
       }
       
     } catch (error) {
-      
+      console.log(error);
+      toast.error(error.message) 
     }
 
   }
@@ -114,7 +131,8 @@ const MyAppointments = () => {
             </div>
             <div></div>
             <div className='flex flex-col gap-2 justify-end'>
-              {!item.cancelled && <button onClick={()=>appointmentRazorpay(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300'>Pay Online</button>}
+              {!item.cancelled && item.payment && <button className='sm:min-w-48 py-2 border rounded text-stone-500 bg-indigo-50'>Paid</button>}
+              {!item.cancelled && !item.payment && <button onClick={()=>appointmentRazorpay(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300'>Pay Online</button>}
               {!item.cancelled && <button onClick={()=>cancelAppointment(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel appointment</button>}
               {item.cancelled && <button className='sm:min-w-48 py-2 border border-red-500 rounded text-red-500'>Appointment cancelled</button>}
             </div>
