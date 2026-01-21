bc73a161edc36cee04a1bcbc039c1394b0e4b577
Sanket01-debug <sanketkansal2001@gmail.com>
2026-01-21 21:39:20 +0530

    feat: created the admin dashboard

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -17,7 +17,7 @@ const AdminContextProvider = (props) => {
     const getAllDoctors = async () => {
         try {
 
-            const {data} = await axios.post(backendUrl + 'api/admin/all-doctors', {}, {headers:{aToken}})
+            const {data} = await axios.post(backendUrl + '/api/admin/all-doctors', {}, {headers:{aToken}})
             if (data.success) {
                 setDoctors(data.doctors)
                 console.log(data.doctors)
@@ -33,7 +33,7 @@ const AdminContextProvider = (props) => {
     const changeAvailability = async (docId) => {
         try {
 
-            const { data } = await axios.post(backendUrl + 'api/admin/change-availability', {docId}, {headers:{aToken}})
+            const { data } = await axios.post(backendUrl + '/api/admin/change-availability', {docId}, {headers:{aToken}})
             if(data.success){
                 toast.success(data.message)
                 getAllDoctors()
