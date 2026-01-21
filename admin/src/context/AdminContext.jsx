ecbc87c1d95cd25b6885b149a046697e4ed31741
Sanket01-debug <sanketkansal2001@gmail.com>
2026-01-21 21:09:12 +0530

    feat: adding API to get dashboard data for admin panel

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -10,6 +10,7 @@ const AdminContextProvider = (props) => {
     const[aToken, setAToken] = useState(localStorage.getItem('aToken')?localStorage.getItem('aToken'):'')
     const[doctors, setDoctors] = useState([])
     const[appointments, setAppointments] = useState([])
+    const[dashData, setDashData] = useState(false)
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
@@ -81,6 +82,23 @@ const AdminContextProvider = (props) => {
 
     }
 
+    const getDashData = async () => {
+        try {
+
+            const {data} = await axios.get(backendUrl+'/api/admin/dashboard',{headers:{aToken}})
+
+            if(data.success){
+                setDashData(data.dashData)
+                console.log(data.dashData)
+            } else {
+                toast.error(data.message)
+            }
+            
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
     const value = {
         aToken, setAToken,
         backendUrl,doctors,
@@ -88,7 +106,8 @@ const AdminContextProvider = (props) => {
         changeAvailability,
         appointments,setAppointments,
         getAllAppointments,
-        cancelAppointment
+        cancelAppointment,
+        dashData,getDashData
     }
 
     return(
